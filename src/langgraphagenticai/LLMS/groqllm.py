2a621e998b66e50325025a28b59ab86fe83d803e
nicolas-sales <nicolas.sales77@gmail.com>
2026-08-31 17:57:40 +0200

    Project done

--- src/langgraphagenticai/LLMS/groqllm.py
+++ src/langgraphagenticai/LLMS/groqllm.py
@@ -4,7 +4,7 @@ from langchain_groq  import ChatGroq
 
 class GroqLLM:
     def __init__(self,user_controls_input):
-        self.user_controls_input=self.user_controls_input
+        self.user_controls_input= user_controls_input
 
     def get_llm_model(self):
         try:
@@ -16,6 +16,6 @@ class GroqLLM:
             llm=ChatGroq(api_key=groq_api_key,model=selected_groq_model)
 
         except Exception as e:
-            raise ValueErrorf(f"Error occured with exception: {e}")
+            raise ValueError(f"Error occured with exception: {e}")
 
         return llm
--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -1,13 +1,12 @@
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, START, END
 from src.langgraphagenticai.state.state import State
-from langgraph.graph import START, END
 from src.langgraphagenticai.nodes.basic_chatbot_node import BasicChatbotNode
 
 
 class GraphBuilder:
     def __init__(self,model):
         self.llm=model
-        self.grap_builder=StateGraph(State)
+        self.graph_builder=StateGraph(State)
 
     def basic_chatbot_build_graph(self):
         """
@@ -20,5 +19,14 @@ class GraphBuilder:
         self.basic_chatbot_node=BasicChatbotNode(self.llm)
 
         self.graph_builder.add_node("chatbot",self.basic_chatbot_node.process)
-        self.grap_builder.add_edge(START,"chatbot")
-        self.grap_builder.add_edge("chatbot",END)
+        self.graph_builder.add_edge(START,"chatbot")
+        self.graph_builder.add_edge("chatbot",END)
+
+    def setup_graph(self, usecase: str):
+        """
+        Sets up the graph for the selected use case.
+        """
+        if usecase == "Basic Chatbot":
+            self.basic_chatbot_build_graph()
+
+        return self.graph_builder.compile()
